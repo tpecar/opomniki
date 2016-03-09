60c01cff50755fdba8302226cc47b7b815039009
Tadej P <tp2664@student.uni-lj.si>
2016-03-09 16:53:51 +0000

    Implementirana prijava

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -1,11 +1,22 @@
 window.addEventListener('load', function() {
 	//stran nalozena
+	// tu lahko definiramo vse funkcije
+	
+	// funkcija za prijavo
+	var izvediPrijavo = function(event) {
+		var uporabnik = document.querySelector("#uporabnisko_ime").value;
+		document.querySelector("#uporabnik").innerHTML = uporabnik;
+		// skrijemo prekrivno okno
+		document.querySelector(".pokrivalo").style.display = "none";
+	}
+	// vezemo na listener gumba
+	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
 		
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
-		
-		for (i = 0; i < opomniki.length; i++) {
+
+		for (var i = 0; i < opomniki.length; i++) {
 			var opomnik = opomniki[i];
 			var casovnik = opomnik.querySelector("span");
 			var cas = parseInt(casovnik.innerHTML);
